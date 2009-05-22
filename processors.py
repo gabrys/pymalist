02d22b90b1d511c3717dc2c1775d14cd901437dc
Piotr Gabryjeluk <piotr@wikidot.com>
2009-05-23 00:58:18 +0200

    Fixed subject modification

--- processors.py
+++ processors.py
@@ -4,6 +4,9 @@ import email.utils
 class NotAuthorized(Exception):
     pass
 
+class NoSuchList(Exception):
+    pass
+
 class DummyProcessor(object):
     def process(self, msg):
         return '', '', msg, []
@@ -24,8 +27,6 @@ class SingleListProcessor():
         if email.utils.parseaddr(sender)[1] not in self.subscribers:
             raise NotAuthorized, sender
         
-        subject = msg['Subject']
-
         try: del(msg['Sender'])
         except: pass
         msg['Sender'] = self.list_mail
@@ -34,9 +35,33 @@ class SingleListProcessor():
         except: pass
         msg['Reply-to'] = self.list_mail
 
-        try: del(msg['Subject'])
-        except: pass
-        msg['Subject'] = self.subject_prefix + subject
+        try:
+            subject = msg['Subject']
+            del(msg['Subject'])
+        except:
+            subject = "(No subject)"
+
+        if not subject.find(subject_prefix):
+            subject = subject_prefix + subject
+
+        msg['Subject'] = subject
 
         return 'list', sender, msg.as_string(), self.subscribers
+
+class MoreListsProcessor():
+    def __init__(self, *args):
+        self.lists = {}
+        for list in args:
+            mail = email.utils.parseaddr(list.list_mail)[1]
+            self.lists[mail] = list
+
+    def process(self, msg_str):
+        try:
+            msg = email.message_from_string(msg_str)
+            to = email.utils.parseaddr(msg['To'])[1]
+            list = self.lists[to]
+        except:
+            raise NoSuchList
+        else:
+            return list.process(msg_str)
 
